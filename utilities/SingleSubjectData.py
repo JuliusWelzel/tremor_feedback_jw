75ee5b9a2e8bddde429cdff319c1ec6ce57addeb
Julius Welzel <julius.welzel@gmail.com>
2022-08-02 16:41:54 +0200

    [ADD] epoch class to prep pupil data

--- utilities/SingleSubjectData.py
+++ utilities/SingleSubjectData.py
@@ -66,13 +66,11 @@ class SubjectData:
             self.srate_ppl = np.nan
             self.per_bad_eye = np.nan
 
-    def epoch(self):
-        print(f"{self.n_epochs:.0f} completed epochs found")
-
     def prep_pupil(self):
-        # find all epochs markers
+        # find all exp epochs markers
         nms_mrk         = [nm_mrk for mrk_ in self.mrk["time_series"] for nm_mrk in mrk_]
-        idxs_eps_end    = [i for i,nm in enumerate(nms_mrk) if 'end_trial' in nm]
+        idx_exp_start   = nms_mrk.index("block1")
+        idxs_eps_end    = [i for i,nm in enumerate(nms_mrk) if 'end_trial' in nm and i > idx_exp_start]
         idxs_eps_start  =[idx -1 for idx in idxs_eps_end]
         times_eps_start = self.mrk['time_stamps'][idxs_eps_start]
         times_eps_end   = self.mrk['time_stamps'][idxs_eps_end]
@@ -97,27 +95,56 @@ class SubjectData:
         
 
 
-class TrialData:
-    
-    def __init__(self):
-        # when the mediapipe is first started, it detects the hands. After that it tries to track the hands
-        # as detecting is more time consuming than tracking. If the tracking confidence goes down than the
-        # specified value then again it switches back to detection
+class Epochs:
+    """Epochs object from numpy array.
 
-        # generic infos
-        self.id = None
-        self.date = None
-        self.time = None
-        self.id = None
-        self.scale = None
-        self.feedback_con = None
-        self.rmse = None
-        self.pow_0_3 = None
-        self.pow_4_12 = None
-        self.ppl_sz_l = None
-        self.ppl_sz_r = None
+    Parameters
+    ----------
+    data : array, shape (n_epochs, n_channels, n_times)
+        The channels' time series for each epoch. See notes for proper units of
+        measure.
+    events : None | array of int, shape (n_events, 3)
+        The events typically returned by the read_events function.
+        If some events don't match the events of interest as specified
+        by event_id, they will be marked as 'IGNORED' in the drop log.
+        If None (default), all event values are set to 1 and event time-samples
+        are set to range(n_epochs).
+    tmin : float
+        Start time before event. If nothing provided, defaults to 0.
+    event_id : int | list of int | dict | None
+        The id of the event to consider. If dict,
+        the keys can later be used to access associated events. Example:
+        dict(auditory=1, visual=3). If int, a dict will be created with
+        the id as string. If a list, all events with the IDs specified
+        in the list are used. If None, all events will be used with
+        and a dict is created with string integer names corresponding
+        to the event id integers.
+    """
+
+    def __init__(self, data, times, events=None, event_id=None):
         
-    def load_data(self,datapath,fname):
+        # prep inputs
+        data    = np.asanyarray(data,dtype=np.float64)
+        times   = np.asanyarray(times,dtype=np.float64)     
+
+        if isinstance(events,list):
+            nms_mrk     = [nm_mrk for mrk_ in events["time_series"] for nm_mrk in mrk_]
+
+        ## check inputs
+        # check input data
+        if  times.shape[0] != data.shape[0]:
+            raise ValueError('Data and times have different amount of samples')
+        if data.ndim == 2 and events and not event_id:
+            raise AttributeError('Specify events to epoch the data in event_id')
+        if data.ndim != 3 and events:
+            raise ValueError('Data must be a 3D array of shape (n_channels, '
+                             'n_samples, n_epochs)')
+        # check input events
+        if not any(events.index(event_id)):
+            raise AttributeError('Event for epoching not present in events')
+
+        
+    def epoch(self, event_id=None, tmin=-0.2, tmax=0.5):
         # extract meta data from fname
         self.id = fname.split('_')[0]
         self.date = datetime.datetime.fromtimestamp(int(float(Path(os.path.join(datapath,fname)).stat().st_ctime))).date()
